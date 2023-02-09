e04c3e505b7579b2c4811e569f7c8701ad1e520f
Simon Foster <simon@funkysi1701.com>
2023-02-09 09:36:01 +0000

    Feature/pro (#207)
    * protected
    
    * static
    
    ---------
    
    Co-authored-by: Simon Foster <simon.foster@optix.co.uk>

--- DevOpsAPI/Program.cs
+++ DevOpsAPI/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace DevOpsAPI
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
